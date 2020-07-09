46861eb210fc3d8c4788fba1d58943756ea5e22f
Aria Jackson <aria.jackson@outlook.com.au>
2020-07-09 20:02:17 +1000

    Initial Commit

---
+++ Assets/Fractal.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fractal : MonoBehaviour {
+
+    public Mesh mesh;
+    public Material material;
+    public float childScale;
+    public float emergenceSpeed = 0.1f;
+    public int maxDepth;
+    private int depth;
+    private bool hasSpawnedChildren = false;
+    private Vector3 initialPosition = new Vector3(0, 0, 0);
+    private Vector3 finalPosition = new Vector3(0,0,0);
+    private float lerpFraction = 1;
+    private float time = 0.0f;
+    public float interpolationPeriod;
+
+    private void Start() {
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+        // if (depth < maxDepth) {
+        //     new GameObject("Fractal Child").
+        //         AddComponent<Fractal>().Initialize(this);
+        // }
+    }
+
+    private void Update() {
+        time += Time.deltaTime;
+        if (lerpFraction < 1) {
+            lerpFraction += time * emergenceSpeed;
+            transform.localPosition = Vector3.Lerp(Vector3.zero, finalPosition, lerpFraction);
+        }
+
+        if (
+            time >= interpolationPeriod && 
+                depth < maxDepth &&
+                 hasSpawnedChildren == false &&
+                 lerpFraction >= 1) {
+
+            time = 0.0f;
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.forward);
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.down);
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.left);
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.right);
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.up);
+            new GameObject("Fractal Child").
+                AddComponent<Fractal>().Initialize(this, Vector3.back);
+            hasSpawnedChildren = true;
+        }
+
+    }
+
+    private void Initialize(Fractal parent, Vector3 direction) {
+        mesh = parent.mesh;
+        material = parent.material;
+        maxDepth = parent.maxDepth;
+        depth = parent.depth + 1;
+        emergenceSpeed = parent.emergenceSpeed;
+        hasSpawnedChildren = false;
+        childScale = parent.childScale;
+        interpolationPeriod = parent.interpolationPeriod;
+        // time = parent.time;
+        lerpFraction = 0;
+        finalPosition = direction * (0.5f + 0.5f * childScale);
+        initialPosition = parent.transform.localPosition;
+        transform.parent = parent.transform;
+        transform.localScale = Vector3.one * childScale;
+    }
+}
---
+++ Assets/Isometric3DScene/TerrainGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour {
+
+
+    /* PUBLIC UNITY INTERFACE */
+    public Mesh mesh;
+    public Material material;
+    public GameObject terrainUnit; //Prefab unit to use as the terrain tiling unit
+    public int chunkWidth = 10; //Chunksize of terrain to load
+    public int blockScale = 1; //Scale of the unit of terrain we are using as blocks
+    public float maxMapHeight = 10;
+    /* END PUBLIC UNITY INTERFACE */
+
+    private float offset;
+
+    private GameObject[,] chunkMap;
+    private FastNoise noise;
+    private float[,] heightMap;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        chunkMap = new GameObject[chunkWidth, chunkWidth];
+        offset = terrainUnit.GetComponent<Renderer>().bounds.size.x;
+
+        noise = new FastNoise(5983);
+        noise.SetNoiseType(FastNoise.NoiseType.Perlin);
+
+        heightMap = GetHeightMap(chunkMap, 5);
+
+
+        for (int x = 0; x < chunkWidth; x++) {
+            for (int y = 0; y < chunkWidth; y++) {
+                Vector3 terrainUnitPosition = new Vector3(x * offset, heightMap[x, y], y * offset);
+                chunkMap[x, y] = Instantiate(terrainUnit, terrainUnitPosition, Quaternion.identity);
+                chunkMap[x, y].transform.SetParent(this.transform);
+            }
+        }
+        gameObject.AddComponent<MeshFilter>().mesh = mesh;
+        gameObject.AddComponent<MeshRenderer>().material = material;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    private float[,] GetHeightMap(GameObject[,] targetMap, int octaves) {
+        float[,] mapToPopulate = new float[chunkMap.GetLength(0), chunkMap.GetLength(1)];
+        for (int oct = 1; oct < octaves+1; oct++) {
+            for (int x = 0; x < mapToPopulate.GetLength(0); x++) {
+                for (int y = 0; y < mapToPopulate.GetLength(1); y++) {
+                    mapToPopulate[x, y] += (20/oct)*(noise.GetPerlin((oct)*x,(oct)*y)+1);
+                }
+            }
+        }
+
+        return mapToPopulate;
+    }
+}
