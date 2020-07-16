b5c1bb1eb8a9410b209b3d77fdd1ba910d59a30e
Aria Jackson <aria.jackson@outlook.com.au>
2020-07-16 20:01:00 +1000

    Switch animatedPerlin to procedural mesh instead of cubes

--- Assets/Isometric3DScene/AnimatedPerlin.cs
+++ Assets/Isometric3DScene/AnimatedPerlin.cs
@@ -1,11 +1,13 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
+using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AnimatedPerlin : MonoBehaviour {
 
     public float animationSpeed = 1f;
-    public int terrainLength = 50;
+    public int terrainDepth = 50;
     public int terrainWidth = 50;
     public float mapAmplitude = 20;
     public int octaves = 10;
@@ -15,10 +17,11 @@ public class AnimatedPerlin : MonoBehaviour {
     private Vector2 animationPosition;
     private FastNoise noise;
 
-    private Vector3[,] newVertices;
-    // private Vector2[] newUV;
+    private Vector3[] newVertices;
+    private Vector2[] newUV;
     private int[] newTris;
 
+    private Mesh mesh;
     private MeshFilter meshFilter;
     private MeshRenderer meshRenderer;
 
@@ -27,36 +30,76 @@ public class AnimatedPerlin : MonoBehaviour {
         noise.SetNoiseType(FastNoise.NoiseType.Perlin);
         animationPosition = new Vector2(0, 0);
 
-        heightMap = new float[terrainWidth, terrainLength];
-
+        heightMap = new float[terrainWidth, terrainDepth];
 
         meshFilter = this.gameObject.AddComponent<MeshFilter>();
+        // GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh = new Mesh();
+        meshFilter.mesh = mesh;
+
         meshRenderer = this.gameObject.AddComponent<MeshRenderer>();
-        newVertices = new Vector3[terrainWidth, terrainLength];
-        //Fix the line below so we don't need a factor to be divisble by 2...
-        newTris = new int[terrainLength * terrainWidth / 2];
+        newVertices = new Vector3[(terrainWidth) * (terrainDepth)];
+        newUV = new Vector2[newVertices.Length];
+        newTris = new int[(terrainDepth) * (terrainWidth) * 6];
+
+        for (int i = 0, x = 0; x < terrainWidth; x++) {
+            for (int y = 0; y < terrainDepth; i++, y++) {
+                newVertices[i] = new Vector3(y, 0, x);
+                newUV[i] = new Vector2(newVertices[i].x, newVertices[i].z);
+
+            }
+        }
+
+        // for (int ti = 0, vi = 0, y = 0; y < terrainDepth; y++, vi++) {
+        //     for (int x = 0; x < terrainWidth; x++, ti += 6, vi++) {
+        //         newTris[ti] = vi;
+        //         newTris[ti + 1] = newTris[ti + 4] = vi + 1;
+        //         newTris[ti + 2] = newTris[ti + 3] = vi + terrainWidth;
+        //         newTris[ti + 5] = vi + terrainWidth + 1;
+        //     }
+        // }
+
+        for (int i = 0, ti = 0; i < terrainWidth - 1; i++) {
+            for (int j = 0; j < terrainDepth - 1; j++, ti += 6) {
+                newTris[ti] = j + terrainWidth * i;
+                newTris[ti + 1] = j + 1 + terrainWidth*i;
+                newTris[ti + 2] = j + (terrainWidth * (i + 1));
+
+                newTris[ti + 3] = j + 1 + terrainWidth * i;
+                newTris[ti + 4] = j + 1 + terrainWidth * (i+1);
+                newTris[ti + 5] = j + terrainWidth * (i+1);
+            }
+        }
+
+        mesh.vertices = newVertices;
+        mesh.triangles = newTris;
+        mesh.uv = newUV;
+        mesh.RecalculateNormals();
     }
 
     void Update() {
         animationPosition += new Vector2(animationSpeed * Time.deltaTime, animationSpeed * Time.deltaTime);
         heightMap = GetAnimatedHeightMap(heightMap, noiseSeed, octaves, mapAmplitude, animationPosition);
-        for (int x = 0; x < newVertices.GetLength(0); x++) {
-            for (int y = 0; y < newVertices.GetLength(1); y++) {
-                newVertices[x, y] = new Vector3(x, heightMap[x, y], y);
+        for (int i = 0, x = 0; x < terrainWidth; x++) {
+            for (int y = 0; y < terrainDepth; i++, y++) {
+                newVertices[i] = new Vector3(x, heightMap[x, y], y);
             }
         }
+        mesh.vertices = newVertices;
+        mesh.RecalculateNormals();
+
+
     }
 
-    private void OnDrawGizmos() {
-        Gizmos.color = Color.green;
-        if (newVertices != null) {
-            for (int x = 0; x < newVertices.GetLength(0); x++) {
-                for (int y = 0; y < newVertices.GetLength(1); y++) {
-                    Gizmos.DrawSphere(newVertices[x, y], 0.2f);
-                }
-            }
-        }
-    }
+    // private void OnDrawGizmos() {
+    //     Gizmos.color = Color.green;
+    //     if (newVertices != null) {
+    //         for (int i = 0; i < newVertices.Length; i++) {
+    //             Gizmos.color = new Color(i, 0, 0);
+    //             Gizmos.DrawSphere(newVertices[i], 0.2f);
+    //         }
+    //     }
+    // }
 
     private float[,] GetAnimatedHeightMap(float[,] map, int seed, int octaves, float amp, Vector2 animationPosition) {
         float[,] mapToPopulate = new float[map.GetLength(0), map.GetLength(1)];
